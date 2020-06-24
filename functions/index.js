3e4c80917b5faa779e1f344fbcd8d0e93dba9df1
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-06-24 03:27:04 -0400

    add logs + fix calling notification

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,13 @@ admin.initializeApp();
 
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors")({ origin: true });
 const client = require("twilio")(functions.config().twilio.sid, functions.config().twilio.auth);
 
 app.use(cors);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 const validateFirebaseIdToken = async (req, res, next) => {
+  console.log("START FIREBASE TOKEN AUTH");
   if (
     (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) &&
     !(req.cookies && req.cookies.__session)
@@ -40,12 +39,14 @@ const validateFirebaseIdToken = async (req, res, next) => {
   } catch (error) {
     console.log("ERROR: " + error);
     res.status(403).send("Unauthorized");
+    return false;
   }
 };
 
 app.use(cors);
-// app.use(validateFirebaseIdToken);
+app.use(validateFirebaseIdToken);
 app.get("/servers", (req, res) => {
+  console.log("GETTING SERVERS");
   client.tokens
     .create()
     .then((token) => {
@@ -55,110 +56,119 @@ app.get("/servers", (req, res) => {
       console.log(error);
       res.status(500).send(error);
     });
+  console.log("FINISHED SERVERS");
 });
 
 /*
 JSON should have props:
 {
-  fromUID, toUID, fromDevice 
+  fromProfile, toUID, roomID
 }
 */
 app.post("/call", async (req, res) => {
+  console.log("CALLING with body: " + req.body);
   if (
-    !req.body.hasOwnProperty("fromUID") ||
+    !req.body.hasOwnProperty("fromProfile") ||
     !req.body.hasOwnProperty("toUID") ||
-    !req.body.hasOwnProperty("fromDevice")
+    !req.body.hasOwnProperty("roomID")
   ) {
+    console.log("ERROR");
     return res.status(400).send("Wrong format");
   }
-  let profile = await admin.database().ref(`profiles/${req.body.fromUID}/`).once("value");
-  if (!profile.exists()) return res.status(404).send("User not found");
-  let fromUser = profile.val();
+
   let devicesToUser = await admin
     .database()
     .ref(`/users/${req.body.toUID}/devicesID/`)
     .once("value");
-
-  devicesToUser.forEach((device) => {
-    messages.push({
-      data: {
-        type: "CALL",
-        fromName: fromUser.name,
-        fromUsername: fromUser.username,
-        fromUID: fromUser.uid,
-        fromDevice: req.body.fromDevice,
-      },
-      token: device.val(),
-    });
-  });
-
-  return admin
-    .messaging()
-    .sendAll(messages)
-    .then((response) => {
-      console.log(response.successCount + " messages were sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  // res.send("HERE is data: " + req.body.param1);
-});
-
-app.post("/accept", (req, res) => {
-  if (!req.body.hasOwnProperty("roomID")) {
-    return res.status(400).message("No RoomID");
+  if (!devicesToUser.hasChildren()) {
+    return res.status(200).send("There are no notification tokens to send to.");
   }
+  const tokens = Object.keys(devicesToUser.val());
+  const tokensToRemove = [];
+  const payload = {
+    data: {
+      type: "CALL",
+      fromName: req.body.fromProfile.name,
+      fromUsername: req.body.fromProfile.username,
+      fromUID: req.body.fromProfile.uid,
+      fromProfilePic: req.body.fromProfile.profilePic,
+      roomID: req.body.roomID,
+    },
+  };
+  const response = await admin.messaging().sendToDevice(tokens, payload);
+  response.results.forEach((result, index) => {
+    const error = result.error;
+    if (error) {
+      console.error("Failure sending notification to", tokens[index], error);
+      if (
+        error.code === "messaging/invalid-registration-token" ||
+        error.code === "messaging/registration-token-not-registered"
+      ) {
+        tokensToRemove.push(devices.ref.child(tokens[index]).remove());
+      }
+    }
+  });
+  console.log("FINISHED CALLING");
+  await Promise.all(tokensToRemove);
+  return res.status(200).send("Finished");
 });
 
 exports.app = functions.https.onRequest(app);
 
-exports.deleteRoom = functions.database
-  .ref("/rooms/{roomID}/users")
-  .onWrite(async (snap, context) => {
-    if (snap.after.val() == null) return false;
-    if (snap.after.val() <= 0) {
-      return admin.database().ref(`/rooms/${context.params.roomID}`).delete();
-    }
-  });
+// exports.deleteRoom = functions.database
+//   .ref("/rooms/{roomID}/users")
+//   .onWrite(async (snap, context) => {
+//     if (snap.after.val() == null) return false;
+//     if (snap.after.val() <= 0) {
+//       return admin.database().ref(`/rooms/${context.params.roomID}`).delete();
+//     }
+//   });
 
 exports.sendFriendRequest = functions.database
   .ref("/users/{uid}/friendRequests/{pushID}")
   .onWrite(async (snap, context) => {
     // console.log(context.params.pushID);
+    console.log("SEND FRIEND REQUEST");
     //Removed
     if (snap.after.val() == null) {
       return false;
     }
     //User containing UID, date sent, accepted value
-    let user = snap.after.val();
+    let request = snap.after.val();
     //Devices
     let devices = await admin
       .database()
       .ref(`/users/${context.params.uid}/devicesID/`)
       .once("value");
 
-    const messages = [];
+    if (!devices.hasChildren()) {
+      return console.log("There are no notification tokens to send to.");
+    }
+    const tokens = Object.keys(devices.val());
+    const tokensToRemove = [];
 
-    devices.forEach((a) => {
-      messages.push({
-        data: {
-          type: "NOTIFICATION",
-          title: "Friend Request",
-          body: "You have a new friend request!",
-        },
-        token: a.val(),
-      });
+    const payload = {
+      data: {
+        type: "NOTIFICATION",
+        title: "Friend Request",
+        body: "You have a new friend request!",
+      },
+    };
+
+    const response = await admin.messaging().sendToDevice(tokens, payload);
+    response.results.forEach((result, index) => {
+      const error = result.error;
+      if (error) {
+        console.error("Failure sending notification to", tokens[index], error);
+        if (
+          error.code === "messaging/invalid-registration-token" ||
+          error.code === "messaging/registration-token-not-registered"
+        ) {
+          tokensToRemove.push(devices.ref.child(tokens[index]).remove());
+        }
+      }
     });
-
-    return admin
-      .messaging()
-      .sendAll(messages)
-      .then((response) => {
-        console.log(response.successCount + " messages were sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    return Promise.all(tokensToRemove);
   });
 
 // let ob = {"users":{"uid":{"friendRequestsSent": {"PUSHID":{"fromUID":"srihari2", "toUID":"SRIHARI"}}}}}
